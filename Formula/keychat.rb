d077853c63c6d634ac49744233584b4509e7e4d4
kcdev <btc001@tuta.io>
2026-03-27 11:47:36 +0800

    Initial tap: keychat formula and README

---
+++ Formula/keychat.rb
@@ -0,0 +1,34 @@
+class Keychat < Formula
+  desc "Encrypted messaging CLI over Nostr with Signal protocol"
+  homepage "https://github.com/keychat-io/keychat-protocol"
+  version "0.1.0"
+  license "MIT"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/keychat-io/keychat-protocol/releases/download/v0.1.0/keychat-aarch64-apple-darwin.tar.gz"
+      sha256 "PLACEHOLDER"
+    else
+      url "https://github.com/keychat-io/keychat-protocol/releases/download/v0.1.0/keychat-x86_64-apple-darwin.tar.gz"
+      sha256 "PLACEHOLDER"
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.arm?
+      url "https://github.com/keychat-io/keychat-protocol/releases/download/v0.1.0/keychat-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "PLACEHOLDER"
+    else
+      url "https://github.com/keychat-io/keychat-protocol/releases/download/v0.1.0/keychat-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "PLACEHOLDER"
+    end
+  end
+
+  def install
+    bin.install "keychat"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/keychat --version")
+  end
+end
---
+++ README.md
@@ -0,0 +1,22 @@
+# Homebrew Tap for Keychat
+
+Homebrew formulae for [Keychat](https://github.com/keychat-io/keychat-protocol) tools.
+
+## Install
+
+```bash
+brew tap keychat-io/tap
+brew install keychat
+```
+
+## Upgrade
+
+```bash
+brew upgrade keychat
+```
+
+## Available Formulae
+
+| Formula | Description |
+|---------|-------------|
+| `keychat` | Encrypted messaging CLI over Nostr with Signal protocol |
